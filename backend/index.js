8fdbe8199235b0c2814daf6536f9939c99eefce5
David Cho <davidcho@Davids-MacBook-Air.local>
2026-04-13 13:27:24 +1000

    added mongodb capabilities - CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 // const http = require('http')
 const express = require('express')
 const app = express()
@@ -6,46 +8,43 @@ app.use(express.json())
 
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(note => Number(note.id))) : 0
-  return String(maxId) + 1
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body || {})
+  console.log('---')
+  next()
 }
 
-app.get('/', (request, response) => {
-  response.json('<h1>Hello World</h1>')
-})
+app.use(requestLogger)
+
+const Note = require('./models/note')
+
+// const generateId = () => {
+//   const maxId = notes.length > 0 ? Math.max(...notes.map(note => Number(note.id))) : 0
+//   return String(maxId) + 1
+// }
+
+// app.get('/', (request, response) => {
+//   response.json("<h1>Hello World</h1>")
+// })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  const note = notes.find(note => note.id === id)
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  Note.findById(id).then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -57,60 +56,65 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note ({
     content: body.content,
     important: body.important || false
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  return note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-// app.put('/api/notes/:id', (request, response) => {
-//   const id = request.params.id
-//   // const notes = notes.map(note => note.id === id ? !note.important : null)
-//   // console.log(id)
-//   // response.status(204).end()
-//   const { content, important } = request.body
-//   notes = notes.map(note => {
-//     if (note.id === id) {
-//       note.content = content
-//       note.important = important
+app.put('/api/notes/:id', (request, response) => {
+  const id = request.params.id
 
-//       console.log(`${note.content} + ${note.important}`)
-//       // console.log(`${content} + ${important}`)
-//       return response.json(note)
-//     }
-//   })
-// })
+  const { content, important } = request.body
+
+  Note.findById(id).then(note => {
+    if (!note) {
+      return response.status(404).end()
+    }
+
+    note.content = content
+    note.important = important
+
+    return note.save().then(updatedNote => {
+      response.json(updatedNote)
+    }).catch(error => next(error))
+  })
+})
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+
+  Note.findByIdAndDelete(id).then(note => {
+    response.status(204).end()
+  }).catch(error => next(error))
 })
 
 /* 
   Middleware
 */
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.use(requestLogger)
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted ID' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
 })
